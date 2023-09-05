d88ee2ea717ad1508bca92727d2fd59a84555d49
Matthew Amosa <amosa.matthew@pm.me>
2023-09-05 00:45:59 -0600

    phone icon fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+// Adds an event listener for the Phone-icon for mobile devices
+document.getElementById('phone-icon').addEventListener('click', function() {
+  window.location.href = 'tel:+8001234567';
+});
+
 // Places just the current year into footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
